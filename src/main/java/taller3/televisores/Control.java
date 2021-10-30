050586fd4184cb107dc01cd893a3e5577cd5ff21
SSR-Med <80341800+SSR-Med@users.noreply.github.com>
2021-10-29 22:02:13 -0500

    Add files via upload

---
+++ src/main/java/taller3/televisores/Control.java
@@ -0,0 +1,75 @@
+package taller3.televisores;
+
+public class Control {
+	TV tv;
+	
+	
+	public void turnOn() {
+		tv.estado = true;
+	}
+	public void turnOff() {
+		tv.estado = false;
+	}
+
+	
+	
+	
+	public void canalUp() {
+		if(tv.estado == true) {
+			if(tv.canal > 0 & tv.canal < 121) {
+				tv.canal = tv.canal + 1;
+			}
+		}
+	}
+	
+	
+	public void canalDown() {
+		if(tv.estado == true) {
+			if(tv.canal > 0 & tv.canal < 121) {
+				tv.canal = tv.canal - 1;
+			}
+		}
+	}
+	
+	
+	public void volumenUp() {
+		if(tv.estado == true) {
+			if(tv.volumen > -1 & tv.volumen < 8) {
+				tv.volumen = tv.volumen + 1;
+			}
+		}
+	}
+	
+	
+	
+	public void volumenDown() {
+		if(tv.estado == true) {
+			if(tv.volumen > -1 & tv.volumen < 8) {
+				tv.volumen = tv.volumen -1;
+			}
+		}
+	}
+	
+	
+	public void setCanal(int canal) {
+		if(tv.estado == true) {
+			if(tv.canal > 0 & tv.canal < 121) {
+				tv.canal = canal;
+			}
+		}
+	}
+	
+	
+	
+	public void enlazar(TV televisor) {
+		this.tv = televisor;
+		tv.control = this;
+	}
+	
+	public TV getTv() {
+		return tv;
+	}
+	public void setTv(TV televisor) {
+		this.tv = televisor;
+	}
+}
